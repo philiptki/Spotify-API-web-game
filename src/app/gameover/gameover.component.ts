48a189577eb7a79e29f7b71fee47709a30bec4e9
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-18 17:03:11 -0500

    Add game over page

---
+++ src/app/gameover/gameover.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameover',
+  templateUrl: './gameover.component.html',
+  styleUrls: ['./gameover.component.css']
+})
+export class GameoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clicked() {
+    console.log("button clicked");
+  }
+}
