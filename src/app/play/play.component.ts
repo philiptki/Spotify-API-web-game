e4e7a4cd9654b601959c34fe26ddabaf129f7d92
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-18 22:43:08 -0500

    Audio playback

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-play',
@@ -12,8 +13,50 @@ export class PlayComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  playing = false;
 
-  clicked() {
-    console.log("song choice clicked")
+  trackIndex = 0;
+
+  tracks = [
+    "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3",
+    "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample2.mp3",
+    "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.m4a",
+  ]
+
+  player: Howl | undefined;
+
+  clicked(index: number) {
+    console.log("song choice clicked");
+    this.trackIndex = index;
+    this.setupPlayer();
+  }
+
+  setupPlayer() {
+    this.player = new Howl({
+      src: [this.tracks[this.trackIndex]],
+      html5: true,
+      autoplay: false,
+      loop: false,
+      volume: 1,
+    });
+  }
+
+  togglePlayback() {
+    if (this.player) {
+      if (this.player.playing()) {
+        this.player.pause();
+        this.playing = false;
+      } else {
+        this.player.play();
+        this.playing = true;
+      }
+    }
+  }
+
+  stopPlayback() {
+    if (this.player) {
+      this.player.stop();
+      this.playing = false;
+    }
   }
 }
