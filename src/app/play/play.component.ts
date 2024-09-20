2ef1ae6a6daba3fd262dc74f820b5feae250086f
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-19 19:31:16 -0500

    Track state and only play mainTrack

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit} from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 import { Howl } from 'howler';
 import { playService } from "../../services/playService";
 import {lowerFirst} from "lodash";
-import { getVolume } from 'src/services/storage';
+import { getVolume, startGame, getGame, getWrong, getPoints, stopGame, setWrong, setPoints } from 'src/services/storage';
 
 @Component({
   selector: 'app-play',
@@ -11,19 +12,7 @@ import { getVolume } from 'src/services/storage';
 })
 export class PlayComponent implements OnInit {
 
-  constructor(private playService: playService) { }
-
-  ngOnInit(): void {
-    this.playService.startRound().then(() => {
-      this.gameStarted = true;
-      this.randomThreeTracks = this.playService.randomThreeTracks;
-      this.mainTrack = this.playService.mainTrack;
-      this.setupTracksPreviewURL()
-      console.log('Round started');
-    }).catch((error) => {
-      console.error('Error starting the round:', error);
-    });
-  }
+  constructor(private playService: playService, private router: Router) { }
 
   playing = false;
 
@@ -37,21 +26,76 @@ export class PlayComponent implements OnInit {
 
   player: Howl | undefined;
 
-  gameStarted = false;
+  roundStarted = false;
 
   randomThreeTracks: any[] = []
 
   mainTrack: any;
+  mainTrackIndex: number = 0;
+
+  ngOnInit(): void {
+    // Check if the user navigated here or if the page was reloaded.
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        const trigger = this.router.getCurrentNavigation()?.trigger;
+        // Continue the game only if the page was automatically reloaded.
+        if (trigger !== 'imperative') {
+          const gameInProgress = getGame();
+          if (gameInProgress) {
+            console.log('Game continued')
+          }
+        } else {
+          startGame();
+          console.log('Game started');
+        }
+      }
+    });
+    this.playService.startRound().then(() => {
+      this.startRound();
+    }).catch((error) => {
+      console.error('Error starting the round:', error);
+    });
+  }
+
+  async startRound() {
+    this.playService.startRound().then(() => {
+      this.roundStarted = true;
+      this.randomThreeTracks = this.playService.randomThreeTracks;
+      this.mainTrack = this.playService.mainTrack;
+      this.mainTrackIndex = this.playService.mainTrackIndex;
+      this.setupTracksPreviewURL()
+      console.log('Round started');
+      console.log(this.mainTrack?.name);
+      this.setupPlayer();
+    }).catch((error) => {
+      console.error('Error starting the round:', error);
+    });
+  }
+
+  nextRound() {
+    this.roundStarted = false;
+    this.startRound();
+  }
+
 
   clicked(index: number) {
     console.log("song choice clicked");
-    this.trackIndex = index;
-    this.setupPlayer();
+    if (index === this.mainTrackIndex) {
+      setPoints(getPoints() + 1);
+    } else {
+      setWrong(getWrong() + 1);
+    }
+    this.nextRound();
+  }
+
+  notPlaying() {
+      console.log("playback ended")
+      this.playing = false;
   }
 
   setupPlayer() {
     this.player = new Howl({
-      src: [this.tracksPreviewURL[this.trackIndex]],
+      src: [this.tracksPreviewURL[this.mainTrackIndex]],
       html5: true,
       autoplay: false,
       loop: false,
@@ -60,6 +104,7 @@ export class PlayComponent implements OnInit {
   }
 
   togglePlayback() {
+    console.log("playback toggled");
     if (this.player) {
       if (this.player.playing()) {
         this.player.pause();
@@ -86,4 +131,31 @@ export class PlayComponent implements OnInit {
         this.randomThreeTracks[2].preview_url
     ]
   }
+
+  //nextRound() {
+  //  const internalRefresh = false;
+  //  this.leavePage(internalRefresh);
+  //  this.router.navigate(["/play"], { skipLocationChange: true });
+  //}
+
+  getWrong() {
+    return getWrong();
+  }
+
+  setWrong(wrong: number) {
+    setWrong(wrong);
+  }
+
+  getPoints() {
+    return getPoints();
+  }
+
+  setPoints(points: number) {
+    setPoints(points);
+  }
+
+  stopGame() {
+    stopGame();
+  }
+
 }
