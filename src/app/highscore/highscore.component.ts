1ec7247a79b0a4e321a642cff1c9788dad3590fb
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-19 23:31:12 -0500

    High score entry

--- src/app/highscore/highscore.component.ts
+++ src/app/highscore/highscore.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { get } from 'lodash';
+import { getFinalScore, setFinalScore, addScore, checkNewHighScore } from 'src/services/storage';
 
 @Component({
   selector: 'app-highscore',
@@ -16,6 +18,16 @@ export class HighscoreComponent implements OnInit {
   }
 
   clicked() {
+    const finalScore = getFinalScore();
     console.log("button clicked");
+    // Ensure the score is greater than zero and is a valid high score.
+    if (finalScore > 0 && checkNewHighScore(finalScore)) {
+      console.log(this.playerName, finalScore)
+      // Add the score to the leaderboard.
+      addScore(this.playerName, finalScore);
+      // Clear the final score out of the temporary storage.
+      // It is now stored in the leaderboard instead.
+      setFinalScore(0);
+    }
   }
 }
